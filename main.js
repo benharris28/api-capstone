bc042eb59430eaf289590e0f867f102f32841ec2
Ben Harris <benjaminharris28@gmail.com>
2019-11-27 22:57:30 -0500

    created new files

---
+++ main.js
@@ -0,0 +1,25 @@
+function getRecipes() {
+    
+
+    fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function getRecipeInstructions() {
+    // Add event listener for click on desired recipe (move to the beginning - right when page loads)
+   // Second fetch (whole code block a second)
+   // Change name of displayResults and call separate function
+
+}
+
+// Event listener for desired recipe
+// On click ('ul') li etc
