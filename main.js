121257e2743a0bde3fc33772c7c2835f7bed7bfa
Ben Harris <benjaminharris28@gmail.com>
2019-11-29 15:15:06 -0500

    updated JS and built out functions

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-function getRecipes() {
-    
+'use strict';
+// Global variables
+
+const apiKey = 'ad90dc536d4b4fa3b7870e7a862dffe7';
+const searchURL = 'https://api.spoonacular.com/recipes/search'
+
+// Takes recipe object and formats into proper string
+// What happens for multiple values in same parameter
+
+function formatRecipes() {
+    console.log('formatRecipes ran');
+  
+}
+
+// Puts parameters into object
+// Calls formatRecipes to insert parameters into properly formatted string
+// puts query string into proper URL
+// Calls first fetch function to get random recipe response
+
+function getRecipes(cuisine, diet, intolerances) {
+    const params = {
+        q: query,
+        cuisine: cuisine,
+        diet: diet,
+        intolerances: intolerances
+    }
+
+    const queryString = formatRecipes(params);
+    const url = searchURL + '?' + queryString;
+
 
     fetch(url)
     .then(response => {
@@ -26,3 +54,14 @@ function getRecipeInstructions() {
 
 
 // watch form or watch forms?
+// getRecipes
+function watchForm() {
+    console.log('watchform ran');
+    $('form').submit(event => {
+        const cuisine = $('#cuisine').val();
+        const diet = $('#diet').val();
+        const intolerances = $('#intolerances').val();
+    });
+}
+
+$(watchForm);
